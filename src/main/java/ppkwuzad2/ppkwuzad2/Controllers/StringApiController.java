cdea9d207ec8a8eb3b9a60c519ea622cc510e67d
Mociej <maciekkocemba@gmail.com>
2021-10-28 13:25:53 +0200

    add stringAnalyse digits, wspaces and other

--- src/main/java/ppkwuzad2/ppkwuzad2/Controllers/StringApiController.java
+++ src/main/java/ppkwuzad2/ppkwuzad2/Controllers/StringApiController.java
@@ -12,14 +12,28 @@ public class StringApiController {
         String result = "String length : " + inputstring.length() + "<br/>";
         int upperCase = 0;
         int lowerCase = 0;
+        int digits = 0;
+        int wspaces = 0;
+        int other = 0;
+
         for (int i = 0; i < inputstring.length(); i++) {
             if (Character.isUpperCase(inputstring.charAt(i)))
                 upperCase++;
-            if (Character.isLowerCase(inputstring.charAt(i)))
+            else if (Character.isLowerCase(inputstring.charAt(i)))
                 lowerCase++;
+            else if(Character.isDigit(inputstring.charAt(i)))
+                digits++;
+            else if(Character.isWhitespace(inputstring.charAt(i)))
+                wspaces++;
+            else
+                other++;
+
         }
         result += "upperCase  : " + upperCase + "<br/>";
         result += "lowerCase  : " + lowerCase + "<br/>";
+        result += "digits  : " + digits + "<br/>";
+        result += "whitespaces  : " + wspaces + "<br/>";
+        result += "special characters  : " + other + "<br/>";
         return result;
     }
 }
