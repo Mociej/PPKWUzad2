71fe40441346e63f306dea5ab08224e82600d8d2
Mociej <maciekkocemba@gmail.com>
2021-10-28 12:37:50 +0200

    intelij basic files

---
+++ src/main/java/ppkwuzad2/ppkwuzad2/Ppkwuzad2Application.java
@@ -0,0 +1,13 @@
+package ppkwuzad2.ppkwuzad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppkwuzad2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppkwuzad2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ppkwuzad2/ppkwuzad2/Ppkwuzad2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ppkwuzad2.ppkwuzad2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppkwuzad2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
